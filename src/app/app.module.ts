12d539925b7df77b944e201c7ee31647bbb04259
Michal Szpak <michal.szpak@softwareplant.com>
2022-01-19 11:36:12 +0100

    NgPoland2021 - Add some more examples of NgChanges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,13 @@ import { NgChangesExample1 } from 'src/app/ng-changes/ng-changes-example-1';
 import { NgChangesExample2 } from 'src/app/ng-changes/ng-changes-example-2';
 import { NgChangesExample3 } from 'src/app/ng-changes/ng-changes-example-3';
 import { OnPushExample1 } from 'src/app/on-push/on-push-example-1';
+import { NgChangesExample6 } from 'src/app/ng-changes/ng-changes-example-6';
+import { NgChangesExample7 } from 'src/app/ng-changes/ng-changes-example-7';
+import { NgChangesExample4 } from 'src/app/ng-changes/ng-changes-example-4';
+import { NgChangesExample5 } from 'src/app/ng-changes/ng-changes-example-5';
+import { NgChangesExample10 } from 'src/app/ng-changes/ng-changes-example-10';
+import { NgChangesExample8 } from 'src/app/ng-changes/ng-changes-example-8';
+import { NgChangesExample9 } from 'src/app/ng-changes/ng-changes-example-9';
 
 @NgModule({
 	declarations: [
@@ -13,6 +20,13 @@ import { OnPushExample1 } from 'src/app/on-push/on-push-example-1';
 		NgChangesExample1,
 		NgChangesExample2,
 		NgChangesExample3,
+		NgChangesExample4,
+		NgChangesExample5,
+		NgChangesExample6,
+		NgChangesExample7,
+		NgChangesExample8,
+		NgChangesExample9,
+		NgChangesExample10,
 		OnPushExample1
 	],
 	imports: [
---
+++ src/app/ng-changes/ng-changes-example-10.ts
@@ -0,0 +1,24 @@
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+	selector: 'ng-changes-example-3',
+	template: '{{ catName }} - {{ dogName }}',
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NgChangesExample10 implements OnChanges {
+
+	@Input()
+	catName: string;
+
+	@Input()
+	dogName: string;
+
+	ngOnChanges(changes: SimpleChanges) {
+		if (changes.catName && changes.catName.currentValue) {
+			this.catName = this.catName.toUpperCase();
+		}
+		if (changes.dogName && changes.dogName.currentValue) {
+			this.dogName = this.dogName.toUpperCase();
+		}
+	}
+}
